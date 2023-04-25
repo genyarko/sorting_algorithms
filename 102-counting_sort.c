d8bcdfdf83e6fcd9788f24fb2f5d206ce2a17687
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-25 16:14:38 +0200

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -7,50 +7,38 @@
  */
 void counting_sort(int *array, size_t size)
 {
-	int *count_arr, *out_arr, max, num;
-	size_t i, j, k;
+	int *count_arr, *out_arr, max, num, j, l;
+	size_t i, k, m, n;
 
 	if (size < 2)
 		return;
 
-	/* Find the maximum value in the array */
 	max = array[0];
 	for (i = 1; i < size; i++)
 		if (array[i] > max)
 			max = array[i];
 
-	/* Allocate memory for the counting array and the output array */
-	count_arr = malloc(sizeof(int) * (max + 1));
+	count_arr = malloc(sizeof(size_t) * (max + 1));
 	out_arr = malloc(sizeof(int) * size);
 
-	/* Initialize the counting array */
 	for (j = 0; j <= max; j++)
 		count_arr[j] = 0;
-
-	/* Count the occurrences of each value */
 	for (k = 0; k < size; k++)
 	{
 		num = array[k];
-		count_arr[num]++;
+		count_arr[num] += 1;
 	}
-
-	/* Accumulate the counts */
-	for (j = 1; j <= max; j++)
-		count_arr[j] += count_arr[j - 1];
-
-	/* Sort the array */
-	for (k = size - 1; k != (size_t)-1; k--)
+	for (l = 1; l <= max; l++)
+		count_arr[l] += count_arr[l - 1];
+	print_array(count_arr, max + 1);
+	for (m = 0; m < size; m++)
 	{
-		num = array[k];
-		out_arr[count_arr[num] - 1] = num;
-		count_arr[num]--;
+		out_arr[count_arr[array[m]] - 1] = array[m];
+		count_arr[array[m]]--;
 	}
+	for (n = 0; n < size; n++)
+		array[n] = out_arr[n];
 
-	/* Copy the sorted array back to the input array */
-	for (i = 0; i < size; i++)
-		array[i] = out_arr[i];
-
-	/* Free the memory */
 	free(count_arr);
 	free(out_arr);
 }
