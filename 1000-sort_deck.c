30c1519008484d53bba1cfdf71082cc1d8c385c2
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-25 23:11:43 +0200

    Update 1000-sort_deck.c

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,60 +1,90 @@
 #include "deck.h"
-#include <stdlib.h>  // for qsort
+#include <stdlib.h>
 
 /**
- * compare_cards - helper function to compare two cards
- * @a: first card to compare
- * @b: second card to compare
+ * compare_cards - Compare two cards
+ * @card1: First card
+ * @card2: Second card
  *
- * Return: negative number if a < b, zero if a == b, positive number if a > b
+ * Return: -1 if card1 < card2, 0 if card1 == card2, 1 if card1 > card2
  */
-static int compare_cards(const void *a, const void *b)
+static int compare_cards(const card_t *card1, const card_t *card2)
 {
-    const card_t *ca = ((const deck_node_t *)a)->card;
-    const card_t *cb = ((const deck_node_t *)b)->card;
+    int value1, value2;
 
-    int value_a, value_b;
-    switch (ca->value[0])
+    if (card1->kind != card2->kind)
+        return (card1->kind - card2->kind);
+
+    switch (card1->value[0])
     {
-        case 'A': value_a = 1; break;
-        case 'J': value_a = 11; break;
-        case 'Q': value_a = 12; break;
-        case 'K': value_a = 13; break;
-        default: value_a = atoi(ca->value); break;
+    case 'A':
+        value1 = 1;
+        break;
+    case '1':
+        value1 = 10;
+        break;
+    case 'J':
+        value1 = 11;
+        break;
+    case 'Q':
+        value1 = 12;
+        break;
+    case 'K':
+        value1 = 13;
+        break;
+    default:
+        value1 = card1->value[0] - '0';
+        break;
     }
-    switch (cb->value[0])
+
+    switch (card2->value[0])
     {
-        case 'A': value_b = 1; break;
-        case 'J': value_b = 11; break;
-        case 'Q': value_b = 12; break;
-        case 'K': value_b = 13; break;
-        default: value_b = atoi(cb->value); break;
+    case 'A':
+        value2 = 1;
+        break;
+    case '1':
+        value2 = 10;
+        break;
+    case 'J':
+        value2 = 11;
+        break;
+    case 'Q':
+        value2 = 12;
+        break;
+    case 'K':
+        value2 = 13;
+        break;
+    default:
+        value2 = card2->value[0] - '0';
+        break;
     }
 
-    int kind_a = ca->kind;
-    int kind_b = cb->kind;
-
-    if (value_a != value_b)
-        return value_a - value_b;
-    else
-        return kind_a - kind_b;
+    return (value1 - value2);
 }
 
 /**
- * sort_deck - sorts a deck of cards
- * @deck: doubly linked list to sort
+ * sort_deck - Sort a deck of cards
+ * @deck: Pointer to the head of the deck
  */
 void sort_deck(deck_node_t **deck)
 {
-    if (!deck || !*deck)
+    size_t i, j;
+    deck_node_t *node1, *node2;
+    const card_t *tmp_card;
+
+    if (!deck || !*deck || !(*deck)->next)
         return;
 
-    qsort(*deck, 52, sizeof(deck_node_t), compare_cards);
-
-    // fix the next and prev pointers of the nodes
-    for (int i = 0; i < 52; i++)
+    for (i = 0, node1 = *deck; i < 51; i++, node1 = node1->next)
     {
-        (*deck)[i].prev = (i == 0 ? NULL : &((*deck)[i-1]));
-        (*deck)[i].next = (i == 51 ? NULL : &((*deck)[i+1]));
+        for (j = i + 1, node2 = node1->next; j < 52; j++, node2 = node2->next)
+        {
+            if (compare_cards(node1->card, node2->card) > 0)
+            {
+                tmp_card = node1->card;
+                node1->card = node2->card;
+                node2->card = tmp_card;
+            }
+        }
     }
 }
