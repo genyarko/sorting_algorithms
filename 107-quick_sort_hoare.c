a163522db33ab498d432c6995e11c1b3f2b7e84d
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-25 22:50:25 +0200

    Update 107-quick_sort_hoare.c

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,63 +1,71 @@
 #include "sort.h"
 
 /**
- * quick_sort_hoare - sorts an array with the Quicksort algorithm
- * @array: array of ints to sort
+ * quick_sort_hoare - sorts an array of integers in ascending order
+ * using the Quick sort algorithm with Hoare partition scheme
+ * @array: pointer to the array to sort
  * @size: size of the array
+ *
+ * Return: void
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
 
-	quick_recursion(array, 0, (int)size - 1, size);
+	quick_sort_recursive(array, 0, size - 1, size);
 }
 
 /**
- * quick_recursion - helper function for Quicksort
- * @array: array to sort
- * @left: index of the left element
- * @right: index of the right element
+ * quick_sort_recursive - recursive function for the Quick sort algorithm
+ * using the Hoare partition scheme
+ * @array: pointer to the array to sort
+ * @left: left index of the subarray to sort
+ * @right: right index of the subarray to sort
  * @size: size of the array
+ *
+ * Return: void
  */
-void quick_recursion(int *array, int left, int right, size_t size)
+void quick_sort_recursive(int *array, int left, int right, size_t size)
 {
-	int piv;
+	int pivot;
 
 	if (left < right)
 	{
-		piv = partition(array, left, right, size);
-		quick_recursion(array, left, piv - 1, size);
-		quick_recursion(array, piv, right, size);
+		pivot = partition(array, left, right, size);
+		quick_sort_recursive(array, left, pivot, size);
+		quick_sort_recursive(array, pivot + 1, right, size);
 	}
 }
 
 /**
- * partition - gives a piv index for Quicksort
- * @array: array to find the piv in
- * @left: index of the left element
- * @right: index of the right element
+ * partition - performs the partition step of the Quick sort algorithm
+ * using the Hoare partition scheme
+ * @array: pointer to the array to sort
+ * @left: left index of the subarray to sort
+ * @right: right index of the subarray to sort
  * @size: size of the array
  *
- * Return: the index of the piv element
+ * Return: index of the pivot element
  */
 int partition(int *array, int left, int right, size_t size)
 {
-	int tmp, pivot = array[right];
-	size_t i, j;
+	int pivot = array[right], tmp;
+	int i = left - 1, j = right + 1;
 
-	i = left - 1;
-	j = right + 1;
 	while (1)
 	{
 		do {
 			i++;
 		} while (array[i] < pivot);
+
 		do {
 			j--;
 		} while (array[j] > pivot);
+
 		if (i >= j)
-			return (i);
+			return (j);
+
 		if (i != j)
 		{
 			tmp = array[i];
@@ -66,5 +74,4 @@ int partition(int *array, int left, int right, size_t size)
 			print_array(array, size);
 		}
 	}
-	return (0);
 }
