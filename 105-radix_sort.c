015f231b4501d53b0c01ed7ebb6c13f9f0e6b006
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-25 21:51:52 +0200

    Update 105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdlib.h>
 
 /**
  * radix_sort - sorts an array following the Radix sort algorithm
@@ -7,52 +8,49 @@
  */
 void radix_sort(int *array, size_t size)
 {
-	int max_num;
-	size_t i, lsd;
+	int max_val, digit;
+	size_t i, j;
+	int *output, *count;
 
 	if (!array || size < 2)
 		return;
 
-	max_num = 0;
-	for (i = 0; i < size; i++)
-		if (array[i] > max_num)
-			max_num = array[i];
+	output = malloc(sizeof(int) * size);
+	count = malloc(sizeof(int) * 10);
 
-	for (lsd = 1; max_num / lsd > 0; lsd *= 10)
+	if (!output || !count)
+		return;
+
+	max_val = array[0];
+	for (i = 1; i < size; i++)
 	{
-		count_sort_LSD(array, size, lsd);
+		if (array[i] > max_val)
+			max_val = array[i];
+	}
+
+	for (digit = 1; max_val / digit > 0; digit *= 10)
+	{
+		for (i = 0; i < 10; i++)
+			count[i] = 0;
+
+		for (i = 0; i < size; i++)
+			count[(array[i] / digit) % 10]++;
+
+		for (i = 1; i < 10; i++)
+			count[i] += count[i - 1];
+
+		for (j = size - 1; (int) j >= 0; j--)
+		{
+			output[count[(array[j] / digit) % 10] - 1] = array[j];
+			count[(array[j] / digit) % 10]--;
+		}
+
+		for (i = 0; i < size; i++)
+			array[i] = output[i];
+
 		print_array(array, size);
 	}
-}
 
-/**
- * count_sort_LSD - count sort with LSD
- * @array: array to sort
- * @size: size of the array
- * @lsd: least significant digit
- */
-void count_sort_LSD(int *array, size_t size, size_t lsd)
-{
-	int count_arr[10] = {0}, *output_array, digit, j;
-	size_t i, k;
-
-	output_array = malloc(sizeof(int) * size);
-
-	for (i = 0; i < size; i++)
-		count_arr[(array[i] / lsd) % 10]++;
-
-	for (j = 1; j < 10; j++)
-		count_arr[j] += count_arr[j - 1];
-
-	for (k = size - 1; (int)k >= 0; k--)
-	{
-		digit = (array[k] / lsd) % 10;
-		output_array[count_arr[digit] - 1] = array[k];
-		count_arr[digit]--;
-	}
-
-	for (i = 0; i < size; i++)
-		array[i] = output_array[i];
-
-	free(output_array);
+	free(output);
+	free(count);
 }
