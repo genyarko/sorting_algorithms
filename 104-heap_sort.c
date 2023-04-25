e7b75d80eb70a5af0a3f0804e57d2636014c5be5
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-25 21:16:46 +0200

    Update 104-heap_sort.c

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,59 +1,58 @@
-#include "sort.h"
-
 /**
- * heap_sort - sorts an array using the Heap sort algorithm
- * @array: array of integers to sort
- * @size: size of the array
+ * heap_sort - sorts an array following the Heap sort algorithm
+ * @array: array of ints to sort
+ * @size: size of the array to sort
  */
 void heap_sort(int *array, size_t size)
 {
-	size_t i;
-	int tmp;
+	int i;
+	int temp;
 
-	if (!array || size < 2)
+	if (size < 2)
 		return;
 
-	for (i = (size / 2) - 1; i < size; i--)
-		heapify(array, size, i, size);
+	for (i = size / 2 - 1; i >= 0; i--)
+		heapify_down(array, size, (size_t)i, size);
 
-	for (i = size - 1; i > 0; i--)
+	for (i = size - 1; i >= 0; i--)
 	{
-		tmp = array[0];
-		array[0] = array[i];
-		array[i] = tmp;
-		print_array(array, size);
-		heapify(array, i, 0, size);
+		temp = array[i];
+		array[i] = array[0];
+		array[0] = temp;
+		if (i != 0)
+			print_array(array, size);
+		heapify_down(array, (size_t)i, 0, size);
 	}
 }
 
 /**
- * heapify - turns an array into a max heap
+ * heapify_down - turns an array in a heap tree
  * @array: array to turn into heap
- * @size: size of the array
- * @root: index of the root of the heap
- * @orig_size: original size of the array
+ * @s: size of the subtree
+ * @root: index of the subtree in the heap
+ * @size: size of the whole array
  */
-void heapify(int *array, size_t size, size_t root, size_t orig_size)
+void heapify_down(int *array, size_t s, size_t root, size_t size)
 {
 	size_t max, left, right;
-	int tmp;
+	int temp;
 
 	max = root;
-	left = (2 * root) + 1;
-	right = (2 * root) + 2;
+	left = (root * 2) + 1;
+	right = (root * 2) + 2;
 
-	if (left < size && array[left] > array[max])
+	if (left < s && array[left] > array[max])
 		max = left;
 
-	if (right < size && array[right] > array[max])
+	if (right < s && array[right] > array[max])
 		max = right;
 
 	if (max != root)
 	{
-		tmp = array[root];
+		temp = array[root];
 		array[root] = array[max];
-		array[max] = tmp;
-		print_array(array, orig_size);
-		heapify(array, size, max, orig_size);
+		array[max] = temp;
+		print_array(array, size);
+		heapify_down(array, s, max, size);
 	}
 }
