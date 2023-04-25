dd41fe8cfdb61401968c8372dfa04facc568455d
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-25 22:11:04 +0200

    Update 106-bitonic_sort.c

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,71 +1,74 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
 
 /**
- * bitonic_sort - Sorts an array of integers using the Bitonic sort algorithm.
- * @array: The array of integers to be sorted.
- * @size: The size of the array to be sorted.
- **/
+ * bitonic_sort - sorts an array following the Bitonic sort algorithm
+ * @array: array of ints to sort
+ * @size: size of the array
+ */
 void bitonic_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
+    if (!array || size < 2)
         return;
 
-    bitonic_sort_helper(array, 0, size, 1);
+    bitonic_recursion(array, 0, size - 1, 1, size);
 }
 
 /**
- * bitonic_sort_helper - Recursive helper function for bitonic_sort.
- * @array: The array of integers to be sorted.
- * @start: The starting index of the subarray to be sorted.
- * @size: The size of the subarray to be sorted.
- * @dir: The direction of the sort (1 for ascending, -1 for descending).
- **/
-void bitonic_sort_helper(int *array, size_t start, size_t size, int dir)
+ * bitonic_recursion - recursive function for bitonic sort
+ * @array: array to sort
+ * @l: index of the left-most element
+ * @r: index of the right-most element
+ * @direction: ascending or descending
+ * @size: size of the array
+ */
+void bitonic_recursion(int *array, int l, int r, int direction, size_t size)
 {
-    if (size > 1)
-    {
-        size_t k = size / 2;
+    int step;
 
-        bitonic_sort_helper(array, start, k, 1);
-        bitonic_sort_helper(array, start + k, k, -1);
-        bitonic_merge(array, start, size, dir);
+    if (r - l >= 1)
+    {
+        step = (r + l) / 2;
+        printf("Merging [%d/%lu] ", r - l + 1, size);
+        if (direction)
+            printf("(UP):\n");
+        else
+            printf("(DOWN):\n");
+        print_array(array + l, r - l + 1);
+        bitonic_recursion(array, l, step, 1, size);
+        bitonic_recursion(array, step + 1, r, 0, size);
+        bitonic_merge(array, l, r, direction);
+        printf("Result [%d/%lu] ", r - l + 1, size);
+        if (direction)
+            printf("(UP):\n");
+        else
+            printf("(DOWN):\n");
+        print_array(array + l, r - l + 1);
     }
 }
 
 /**
- * bitonic_merge - Merges two subarrays in a bitonic manner.
- * @array: The array of integers to be sorted.
- * @start: The starting index of the subarray to be merged.
- * @size: The size of the subarray to be merged.
- * @dir: The direction of the sort (1 for ascending, -1 for descending).
- **/
-void bitonic_merge(int *array, size_t start, size_t size, int dir)
+ * bitonic_merge - sorts and merges a sequence in ascending or descending order
+ * @array: array to sort
+ * @l: index of the left-most element
+ * @r: index of the right-most element
+ * @direction: ascending or descending
+ */
+void bitonic_merge(int *array, int l, int r, int direction)
 {
-    if (size > 1)
-    {
-        size_t k = size / 2, i;
+    int tmp, i, step = (l + r) / 2, mid = (r - l + 1) / 2;
 
-        for (i = start; i < start + k; i++)
+    if (r - l >= 1)
+    {
+        for (i = l; i < l + mid; i++)
         {
-            if (dir == (array[i] > array[i + k]))
-                swap(&array[i], &array[i + k]), printf("Swap %d <-> %d\n", array[i], array[i + k]);
+            if (direction == (array[i] > array[i + mid]))
+            {
+                tmp = array[i + mid];
+                array[i + mid] = array[i];
+                array[i] = tmp;
+            }
         }
-
-        bitonic_merge(array, start, k, dir);
-        bitonic_merge(array, start + k, k, dir);
+        bitonic_merge(array, l, step, direction);
+        bitonic_merge(array, step + 1, r, direction);
     }
-}
-
-/**
- * swap - Swaps two integer values.
- * @a: The first integer to be swapped.
- * @b: The second integer to be swapped.
- **/
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
 }
