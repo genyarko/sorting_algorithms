8ac06fda19025d9926736196c2a4556021285315
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-25 21:48:56 +0200

    Update 105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -7,19 +7,18 @@
  */
 void radix_sort(int *array, size_t size)
 {
-	int max = 0;
+	int max_num;
 	size_t i, lsd;
 
 	if (!array || size < 2)
 		return;
 
-	/* Find maximum value in array */
+	max_num = 0;
 	for (i = 0; i < size; i++)
-		if (array[i] > max)
-			max = array[i];
+		if (array[i] > max_num)
+			max_num = array[i];
 
-	/* Sort array for each digit */
-	for (lsd = 1; max / lsd > 0; lsd *= 10)
+	for (lsd = 1; max_num / lsd > 0; lsd *= 10)
 	{
 		count_sort_LSD(array, size, lsd);
 		print_array(array, size);
@@ -34,18 +33,26 @@ void radix_sort(int *array, size_t size)
  */
 void count_sort_LSD(int *array, size_t size, size_t lsd)
 {
-	int count_arr[10] = {0}, l, m, temp;
-	size_t k, n;
+	int count_arr[10] = {0}, *output_array, digit, j;
+	size_t i, k;
 
-	for (k = 0; k < size; k++)
-		count_arr[(array[k] / lsd) % 10]++;
+	output_array = malloc(sizeof(int) * size);
 
-	for (l = 1; l < 10; l++)
-		count_arr[l] += count_arr[l - 1];
+	for (i = 0; i < size; i++)
+		count_arr[(array[i] / lsd) % 10]++;
 
-	for (m = size - 1; m >= 0; m--)
+	for (j = 1; j < 10; j++)
+		count_arr[j] += count_arr[j - 1];
+
+	for (k = size - 1; (int)k >= 0; k--)
 	{
-		temp = array[m];
-		array[--count_arr[(temp / lsd) % 10]] = temp;
+		digit = (array[k] / lsd) % 10;
+		output_array[count_arr[digit] - 1] = array[k];
+		count_arr[digit]--;
 	}
+
+	for (i = 0; i < size; i++)
+		array[i] = output_array[i];
+
+	free(output_array);
 }
