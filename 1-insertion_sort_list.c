91e462296dc5ce92145390e27c8f06c594c2b22c
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-25 13:17:24 +0200

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,59 +1,36 @@
+#include <stdio.h>
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts doubly linked list of ints using Insertion sort
- * @list: lst to sort
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm
+ *
+ * @list: The list to be sorted
  */
-
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *sorted, *current, *next;
+    listint_t *current, *temp;
 
-	current = *list;
-	sorted = NULL;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-	while (current)
-	{
-		next = current->next;
-		current->prev = current->next = NULL;
-		sortedInsert(&sorted, &current);
-		current = next;
-
-		print_list(sorted);
-	}
-	*list = sorted;
+    current = (*list)->next;
+    while (current != NULL)
+    {
+        temp = current->prev;
+        while (temp != NULL && temp->n > current->n)
+        {
+            temp->next = current->next;
+            if (temp->prev != NULL)
+                temp->prev->next = current;
+            current->prev = temp->prev;
+            temp->prev = current;
+            current->next = temp;
+            temp = current->prev;
+            if (current->prev == NULL)
+                *list = current;
+            print_list(*list);
+        }
+        current = current->next;
+    }
 }
 
-/**
- * sortedInsert - function for inserting nodes in sorted order
- * @head_ref: reference to stating noe for comparison
- * @newNode: pointer to next node to compare
- */
-
-void sortedInsert(listint_t  **head_ref, listint_t  **newNode)
-{
-	listint_t *current;
-
-	if (*head_ref == NULL)
-		*head_ref = *newNode;
-	else if ((*head_ref)->n >= (*newNode)->n)
-	{
-		(*newNode)->next = *head_ref;
-		(*newNode)->next->prev = *newNode;
-		*head_ref = *newNode;
-	}
-	else
-	{
-		current = *head_ref;
-
-		while (current->next && current->next->n < (*newNode)->n)
-			current = current->next;
-
-		(*newNode)->next = current->next;
-
-		if (current->next != NULL)
-			(*newNode)->next->prev = *newNode;
-		current->next = *newNode;
-		(*newNode)->prev = current;
-	}
-}
