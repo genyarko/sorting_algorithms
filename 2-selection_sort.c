425ef8ebda525ca819cb6b144559300c8f07cca9
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-25 13:21:36 +0200

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,40 +1,31 @@
+#include <stdio.h>
 #include "sort.h"
 
 /**
- * selection_sort -  sorts integers in ascending order using Selection sort
- * @array: array to sort
- * @size: size of array
+ * selection_sort - Sorts an array of integers in ascending order
+ *                  using the selection sort algorithm
+ *
+ * @array: The array of integers to be sorted
+ * @size: The size of the array
  */
-
 void selection_sort(int *array, size_t size)
 {
-	size_t pass;
+    size_t i, j, min_idx;
 
-	for (pass = 0; pass < size - 1; pass++)
-	{
-		int min_index = pass;
-		size_t i;
-
-		for (i = pass + 1; i < size; i++)
-		{
-			if (array[i] < array[min_index])
-				min_index = i;
-		}
-		swap(&array[min_index], &array[pass]);
-		print_array(array, size);
-	}
-}
-
-/**
- * swap - function for swapping values stored in an array
- * @a: first value to swap
- * @b: second value to swap
- */
-
-void swap(int *a, int *b)
-{
-	int tmp = *a;
-
-	*a = *b;
-	*b = tmp;
+    for (i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+                min_idx = j;
+        }
+        if (min_idx != i)
+        {
+            int tmp = array[i];
+            array[i] = array[min_idx];
+            array[min_idx] = tmp;
+            print_array(array, size);
+        }
+    }
 }
