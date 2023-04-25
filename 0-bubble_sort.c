41cde766fae95f25df9a37d4c2fc10501ecec2dd
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-25 12:35:22 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,17 +1,31 @@
-void bubble_sort(int *array, size_t size) {
-    for (size_t i = 0; i < size - 1; i++) {
-        for (size_t j = 0; j < size - 1 - i; j++) {
-            if (array[j] > array[j + 1]) {
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using the Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: The size of the array
+ *
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - 1 - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                /* Swap the adjacent elements */
                 int temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
+
+                /* Print the array after each swap */
+                printf("Array after swapping elements: ");
+                for (size_t k = 0; k < size; k++)
+                    printf("%d ", array[k]);
+                printf("\n");
             }
-            // print the array after each swap
-            printf("Array after swapping elements: ");
-            for (size_t k = 0; k < size; k++) {
-                printf("%d ", array[k]);
-            }
-            printf("\n");
         }
     }
 }
