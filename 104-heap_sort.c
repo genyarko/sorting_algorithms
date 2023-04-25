31a05d397b97bf1568234287f0409fffe4339778
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-25 21:20:59 +0200

    Update 104-heap_sort.c

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,19 +1,20 @@
 #include "sort.h"
 
 /**
- * heap_sort - sorts an array following the Heap sort algorithm
- * @array: array of ints to sort
- * @size: size of the array to sort
+ * heap_sort - sorts an array using the Heap sort algorithm
+ * @array: array of integers to sort
+ * @size: size of the array
  */
 void heap_sort(int *array, size_t size)
 {
-	int i, tmp;
+	size_t i;
+	int tmp;
 
-	if (size < 2)
+	if (!array || size < 2)
 		return;
 
-	for (i = size / 2 - 1; i >= 0; i--)
-		heapify_down(array, size, i, size);
+	for (i = (size / 2) - 1; i < size; i--)
+		heapify(array, size, i, size);
 
 	for (i = size - 1; i > 0; i--)
 	{
@@ -21,25 +22,25 @@ void heap_sort(int *array, size_t size)
 		array[0] = array[i];
 		array[i] = tmp;
 		print_array(array, size);
-		heapify_down(array, (size_t)i, 0, size);
+		heapify(array, i, 0, size);
 	}
 }
 
 /**
- * heapify_down - turns an array into a heap tree
+ * heapify - turns an array into a max heap
  * @array: array to turn into heap
- * @size: size of the heap
- * @root: index of the root of the subtree
+ * @size: size of the array
+ * @root: index of the root of the heap
  * @orig_size: original size of the array
  */
-void heapify_down(int *array, size_t size, size_t root, size_t orig_size)
+void heapify(int *array, size_t size, size_t root, size_t orig_size)
 {
 	size_t max, left, right;
 	int tmp;
 
 	max = root;
-	left = 2 * root + 1;
-	right = 2 * root + 2;
+	left = (2 * root) + 1;
+	right = (2 * root) + 2;
 
 	if (left < size && array[left] > array[max])
 		max = left;
@@ -53,6 +54,7 @@ void heapify_down(int *array, size_t size, size_t root, size_t orig_size)
 		array[root] = array[max];
 		array[max] = tmp;
 		print_array(array, orig_size);
-		heapify_down(array, size, max, orig_size);
+		heapify(array, size, max, orig_size);
 	}
 }
+
