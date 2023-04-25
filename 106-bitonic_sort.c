0e12526e6ba6b6829b6b8ddce38903c7d26d3371
genyarko <115112347+genyarko@users.noreply.github.com>
2023-04-25 21:59:18 +0200

    Update 106-bitonic_sort.c

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -3,69 +3,78 @@
 #include "sort.h"
 
 /**
- * bitonic_sort - sorts an array of integers in ascending order using the Bitonic sort algorithm
- * @array: array to sort
+ * bitonic_merge - recursive function to merge two bitonic sequences in
+ * descending and ascending orders
+ * @array: array of integers
+ * @size: size of the array
+ * @start: starting index of the sequence
+ * @count: size of the sequence
+ * @direction: direction of sorting, 1 for ascending, 0 for descending
+ */
+void bitonic_merge(int *array, size_t size, size_t start, size_t count, int direction)
+{
+    size_t i, j, k, n;
+    int tmp;
+
+    if (count > 1)
+    {
+        size_t m = count / 2;
+
+        printf("Merging [%lu, %lu] (%s):\n", start, start + count - 1, direction ? "UP" : "DOWN");
+        print_array(array + start, count);
+
+        for (i = start, j = start + m; j < start + count; i++, j++)
+        {
+            if ((array[i] > array[j]) == direction)
+            {
+                tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+
+                printf("Swapped elements\n");
+                print_array(array + start, count);
+            }
+        }
+
+        bitonic_merge(array, size, start, m, direction);
+        bitonic_merge(array, size, start + m, m, direction);
+    }
+}
+
+/**
+ * bitonic_sort_recursive - recursive function to sort a bitonic sequence
+ * in ascending and descending orders
+ * @array: array of integers
+ * @size: size of the array
+ * @start: starting index of the sequence
+ * @count: size of the sequence
+ * @direction: direction of sorting, 1 for ascending, 0 for descending
+ */
+void bitonic_sort_recursive(int *array, size_t size, size_t start, size_t count, int direction)
+{
+    size_t m = count / 2;
+
+    if (count > 1)
+    {
+        printf("Merging [%lu, %lu] (%s):\n", start, start + count - 1, direction ? "UP" : "DOWN");
+        print_array(array + start, count);
+
+        bitonic_sort_recursive(array, size, start, m, 1);
+        bitonic_sort_recursive(array, size, start + m, m, 0);
+        bitonic_merge(array, size, start, count, direction);
+    }
+}
+
+/**
+ * bitonic_sort - sorts an array of integers in ascending order
+ * using the Bitonic sort algorithm
+ * @array: array of integers
  * @size: size of the array
  */
 void bitonic_sort(int *array, size_t size)
 {
-    if (!array || size < 2)
-        return;
-
-    bitonic_sort_recursive(array, 0, size, 1);
-}
-
-/**
- * bitonic_sort_recursive - recursive helper function for bitonic_sort
- * @array: array to sort
- * @start: start index of subarray to sort
- * @size: size of the subarray to sort
- * @dir: sort direction (1 for ascending, 0 for descending)
- */
-void bitonic_sort_recursive(int *array, size_t start, size_t size, int dir)
-{
-    if (size > 1)
+    if (array && size > 1 && ((size & (size - 1)) == 0))
     {
-        size_t mid = size / 2;
-        bitonic_merge(array, start, mid, dir);
-        bitonic_sort_recursive(array, start, mid, dir);
-        bitonic_sort_recursive(array, start + mid, mid, !dir);
+        bitonic_sort_recursive(array, size, 0, size, 1);
     }
-}
-
-/**
- * bitonic_merge - merges two subarrays in bitonic order
- * @array: array to sort
- * @start: start index of the first subarray
- * @size: size of each subarray
- * @dir: sort direction (1 for ascending, 0 for descending)
- */
-void bitonic_merge(int *array, size_t start, size_t size, int dir)
-{
-    if (size > 1)
-    {
-        size_t i, j, k;
-        size_t mid = start + size / 2;
-
-        for (i = start; i < mid; i++)
-        {
-            if ((array[i] > array[mid + i - start]) == dir)
-                swap(&array[i], &array[mid + i - start]);
-        }
-
-        bitonic_merge(array, start, size / 2, dir);
-        bitonic_merge(array, mid, size / 2, dir);
-    }
-}
-
-/**
- * swap - swaps two integers
- * @a: pointer to first integer
- * @b: pointer to second integer
- */
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
 }
